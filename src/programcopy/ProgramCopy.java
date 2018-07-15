f37adbe5db550fb1ebf54f087249a9d3f7be6627
Sayid Alwi Al-Attas <38781603+alwi1010@users.noreply.github.com>
2018-07-15 23:56:21 +0800

    Add files via upload

---
+++ src/programcopy/ProgramCopy.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programcopy;
+
+/**
+ *
+ * @author Pro Sawi
+ */
+public class ProgramCopy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FormCopy tampil = new FormCopy();
+        tampil.setVisible(true);
+        tampil.setLocationRelativeTo(null);
+    }
+    
+}
